1619a4f028ef5bcd40428585b1eea35f60975102
yeongbin <baack7700@hanyang.ac.kr>
2023-07-31 18:23:11 +0900

    return the c_api file

--- c_api.c
+++ c_api.c
@@ -19,52 +19,50 @@ SST_ctx_t *init_SST(char *config_path) {
     return ctx;
 }
 
-session_key_list_t* get_session_key(SST_ctx_t* ctx,
-    session_key_list_t* existing_s_key_list) {
+session_key_list_t *get_session_key(SST_ctx_t *ctx,
+                                    session_key_list_t *existing_s_key_list) {
     if (existing_s_key_list != NULL) {
         if (check_session_key_list_addable(ctx->config->numkey,
-            existing_s_key_list)) {
+                                           existing_s_key_list)) {
             printf("Unable to get_session_key().\n");
             return existing_s_key_list;
         }
     }
-    session_key_list_t* earned_s_key_list;
-    if (strcmp((const char*)ctx->config->network_protocol, "TCP") == 0) {
+    session_key_list_t *earned_s_key_list;
+    if (strcmp((const char *)ctx->config->network_protocol, "TCP") == 0) {
         earned_s_key_list = send_session_key_req_via_TCP(ctx);
-    }
-    else if (strcmp((const char*)ctx->config->network_protocol, "UDP") ==
-        0) {
+    } else if (strcmp((const char *)ctx->config->network_protocol, "UDP") ==
+               0) {
         earned_s_key_list = send_session_key_req_via_UDP(ctx);
     }
 
     if (existing_s_key_list == NULL) {
         return earned_s_key_list;
-    }
-    else {
+    } else {
         append_session_key_list(existing_s_key_list, earned_s_key_list);
         free_session_key_list_t(earned_s_key_list);
         return existing_s_key_list;
     }
 }
 
-SST_session_ctx_t* secure_connect_to_server(session_key_t* s_key,
-    SST_ctx_t* ctx) {
+SST_session_ctx_t *secure_connect_to_server(session_key_t *s_key,
+                                            SST_ctx_t *ctx) {
     // Initialize SST_session_ctx_t
-    SST_session_ctx_t* session_ctx = malloc(sizeof(SST_session_ctx_t));
+    SST_session_ctx_t *session_ctx = malloc(sizeof(SST_session_ctx_t));
     session_ctx->received_seq_num = 0;
     session_ctx->sent_seq_num = 0;
 
     int sock;
-    connect_as_client((const char*)ctx->config->entity_server_ip_addr,
-        (const char*)ctx->config->entity_server_port_num, &sock);
+    connect_as_client((const char *)ctx->config->entity_server_ip_addr,
+                      (const char *)ctx->config->entity_server_port_num, &sock);
     unsigned char entity_nonce[HS_NONCE_SIZE];
     unsigned int parsed_buf_length;
-    unsigned char* parsed_buf =
+    unsigned char *parsed_buf =
         parse_handshake_1(s_key, entity_nonce, &parsed_buf_length);
     unsigned char sender_HS_1[MAX_HS_BUF_LENGTH];
     unsigned int sender_HS_1_length;
     make_sender_buf(parsed_buf, parsed_buf_length, SKEY_HANDSHAKE_1,
-        sender_HS_1, &sender_HS_1_length);
+                    sender_HS_1, &sender_HS_1_length);
     write(sock, sender_HS_1, sender_HS_1_length);
     free(parsed_buf);
     entity_client_state = HANDSHAKE_1_SENT;
@@ -75,7 +73,7 @@ SST_session_ctx_t* secure_connect_to_server(session_key_t* s_key,
         read(sock, received_buf, sizeof(received_buf));
     unsigned char message_type;
     unsigned int data_buf_length;
-    unsigned char* data_buf = parse_received_message(
+    unsigned char *data_buf = parse_received_message(
         received_buf, received_buf_length, &message_type, &data_buf_length);
     if (message_type == SKEY_HANDSHAKE_2) {
         if (entity_client_state != HANDSHAKE_1_SENT) {
@@ -84,12 +82,12 @@ SST_session_ctx_t* secure_connect_to_server(session_key_t* s_key,
                 "disconnecting...\n");
         }
         unsigned int parsed_buf_length;
-        unsigned char* parsed_buf = check_handshake_2_send_handshake_3(
+        unsigned char *parsed_buf = check_handshake_2_send_handshake_3(
             data_buf, data_buf_length, entity_nonce, s_key, &parsed_buf_length);
         unsigned char sender_HS_2[MAX_HS_BUF_LENGTH];
         unsigned int sender_HS_2_length;
         make_sender_buf(parsed_buf, parsed_buf_length, SKEY_HANDSHAKE_3,
-            sender_HS_2, &sender_HS_2_length);
+                        sender_HS_2, &sender_HS_2_length);
         write(sock, sender_HS_2, sender_HS_2_length);
         free(parsed_buf);
         update_validity(s_key);
@@ -101,10 +99,10 @@ SST_session_ctx_t* secure_connect_to_server(session_key_t* s_key,
     return session_ctx;
 }
 
-SST_session_ctx_t* server_secure_comm_setup(
-    SST_ctx_t* ctx, int clnt_sock, session_key_list_t* existing_s_key_list) {
+SST_session_ctx_t *server_secure_comm_setup(
+    SST_ctx_t *ctx, int clnt_sock, session_key_list_t *existing_s_key_list) {
     // Initialize SST_session_ctx_t
-    SST_session_ctx_t* session_ctx = malloc(sizeof(SST_session_ctx_t));
+    SST_session_ctx_t *session_ctx = malloc(sizeof(SST_session_ctx_t));
 
     session_ctx->received_seq_num = 0;
     session_ctx->sent_seq_num = 0;
@@ -113,7 +111,7 @@ SST_session_ctx_t* server_secure_comm_setup(
     entity_server_state = IDLE;
     unsigned char server_nonce[HS_NONCE_SIZE];
 
-    session_key_t* s_key;
+    session_key_t *s_key;
 
     if (entity_server_state == IDLE) {
         unsigned char received_buf[MAX_HS_BUF_LENGTH];
@@ -121,7 +119,7 @@ SST_session_ctx_t* server_secure_comm_setup(
             read(clnt_sock, received_buf, HANDSHAKE_1_LENGTH);
         unsigned char message_type;
         unsigned int data_buf_length;
-        unsigned char* data_buf = parse_received_message(
+        unsigned char *data_buf = parse_received_message(
             received_buf, received_buf_length, &message_type, &data_buf_length);
         if (message_type == SKEY_HANDSHAKE_1) {
             printf("received session key handshake1\n");
@@ -148,13 +146,12 @@ SST_session_ctx_t* server_secure_comm_setup(
             }
             if (session_key_found >= 0) {
                 s_key = &existing_s_key_list->s_key[session_key_found];
-            }
-            else if (session_key_found == -1) {
+            } else if (session_key_found == -1) {
                 // WARNING: The following line overwrites the purpose.
-                sprintf(ctx->config->purpose[ctx->purpose_index], "{\"keyId\":%d}",
-                    expected_key_id_int);
+                sprintf(ctx->config->purpose, "{\"keyId\":%d}",
+                        expected_key_id_int);
 
-                session_key_list_t* s_key_list;
+                session_key_list_t *s_key_list;
                 s_key_list = send_session_key_request_check_protocol(
                     ctx, expected_key_id);
                 s_key = s_key_list->s_key;
@@ -168,14 +165,14 @@ SST_session_ctx_t* server_secure_comm_setup(
                     "HANDSHAKE_1_RECEIVED, disconnecting...");
             }
             unsigned int parsed_buf_length;
-            unsigned char* parsed_buf = check_handshake1_send_handshake2(
+            unsigned char *parsed_buf = check_handshake1_send_handshake2(
                 data_buf, data_buf_length, server_nonce, s_key,
                 &parsed_buf_length);
 
             unsigned char sender[MAX_HS_BUF_LENGTH];
             unsigned int sender_length;
             make_sender_buf(parsed_buf, parsed_buf_length, SKEY_HANDSHAKE_2,
-                sender, &sender_length);
+                            sender, &sender_length);
             write(clnt_sock, sender, sender_length);
             free(parsed_buf);
             printf("switching to HANDSHAKE_2_SENT\n");
@@ -188,7 +185,7 @@ SST_session_ctx_t* server_secure_comm_setup(
             read(clnt_sock, received_buf, HANDSHAKE_3_LENGTH);
         unsigned char message_type;
         unsigned int data_buf_length;
-        unsigned char* data_buf = parse_received_message(
+        unsigned char *data_buf = parse_received_message(
             received_buf, received_buf_length, &message_type, &data_buf_length);
         if (message_type == SKEY_HANDSHAKE_3) {
             printf("received session key handshake3!\n");
@@ -198,7 +195,7 @@ SST_session_ctx_t* server_secure_comm_setup(
                     "disconnecting...\n");
             }
             unsigned int decrypted_length;
-            unsigned char* decrypted = symmetric_decrypt_authenticate(
+            unsigned char *decrypted = symmetric_decrypt_authenticate(
                 data_buf, data_buf_length, s_key->mac_key, MAC_KEY_SIZE,
                 s_key->cipher_key, CIPHER_KEY_SIZE, AES_CBC_128_IV_SIZE,
                 &decrypted_length);
@@ -206,13 +203,12 @@ SST_session_ctx_t* server_secure_comm_setup(
             parse_handshake(decrypted, &hs);
             free(decrypted);
             // compare my_nonce and received_nonce
-            if (strncmp((const char*)hs.reply_nonce,
-                (const char*)server_nonce, HS_NONCE_SIZE) != 0) {
+            if (strncmp((const char *)hs.reply_nonce,
+                        (const char *)server_nonce, HS_NONCE_SIZE) != 0) {
                 error_handling(
                     "Comm init failed: server NOT verified, nonce NOT matched, "
                     "disconnecting...\n");
-            }
-            else {
+            } else {
                 printf("server authenticated/authorized by solving nonce!\n");
             }
             update_validity(s_key);
@@ -224,8 +220,8 @@ SST_session_ctx_t* server_secure_comm_setup(
     }
 }
 
-void* receive_thread(void* SST_session_ctx) {
-    SST_session_ctx_t* session_ctx = (SST_session_ctx_t*)SST_session_ctx;
+void *receive_thread(void *SST_session_ctx) {
+    SST_session_ctx_t *session_ctx = (SST_session_ctx_t *)SST_session_ctx;
     unsigned char received_buf[MAX_PAYLOAD_LENGTH];
     unsigned int received_buf_length = 0;
     while (1) {
@@ -244,15 +240,15 @@ void* receive_thread(void* SST_session_ctx) {
     }
 }
 
-void* receive_thread_read_one_each(void* SST_session_ctx) {
-    SST_session_ctx_t* session_ctx = (SST_session_ctx_t*)SST_session_ctx;
+void *receive_thread_read_one_each(void *SST_session_ctx) {
+    SST_session_ctx_t *session_ctx = (SST_session_ctx_t *)SST_session_ctx;
     unsigned char data_buf[MAX_PAYLOAD_LENGTH];
     unsigned int data_buf_length = 0;
     while (1) {
         unsigned char message_type;
         if (1 != read_header_return_data_buf_pointer(session_ctx->sock,
-            &message_type, data_buf,
-            &data_buf_length)) {
+                                                     &message_type, data_buf,
+                                                     &data_buf_length)) {
             return 0;
         }
         if (message_type == SECURE_COMM_MSG) {
@@ -261,43 +257,43 @@ void* receive_thread_read_one_each(void* SST_session_ctx) {
     }
 }
 
-void receive_message(unsigned char* received_buf,
-    unsigned int received_buf_length,
-    SST_session_ctx_t* session_ctx) {
+void receive_message(unsigned char *received_buf,
+                     unsigned int received_buf_length,
+                     SST_session_ctx_t *session_ctx) {
     unsigned char message_type;
     unsigned int data_buf_length;
-    unsigned char* data_buf = parse_received_message(
+    unsigned char *data_buf = parse_received_message(
         received_buf, received_buf_length, &message_type, &data_buf_length);
     if (message_type == SECURE_COMM_MSG) {
         print_received_message(data_buf, data_buf_length, session_ctx);
     }
 }
 
-unsigned char* return_decrypted_buf(unsigned char* received_buf,
-    unsigned int received_buf_length,
-    SST_session_ctx_t* session_ctx) {
+unsigned char *return_decrypted_buf(unsigned char *received_buf,
+                                    unsigned int received_buf_length,
+                                    SST_session_ctx_t *session_ctx) {
     unsigned char message_type;
     unsigned int data_buf_length;
-    unsigned char* data_buf = parse_received_message(
+    unsigned char *data_buf = parse_received_message(
         received_buf, received_buf_length, &message_type, &data_buf_length);
     if (message_type == SECURE_COMM_MSG) {
         return decrypt_received_message(data_buf, data_buf_length, session_ctx);
     }
 }
 
-void send_secure_message(char* msg, unsigned int msg_length,
-    SST_session_ctx_t* session_ctx) {
+void send_secure_message(char *msg, unsigned int msg_length,
+                         SST_session_ctx_t *session_ctx) {
     if (check_session_key_validity(&session_ctx->s_key)) {
         error_handling("Session key expired!\n");
     }
     unsigned char buf[SEQ_NUM_SIZE + msg_length];
     memset(buf, 0, SEQ_NUM_SIZE + msg_length);
     write_in_n_bytes(session_ctx->sent_seq_num, SEQ_NUM_SIZE, buf);
-    memcpy(buf + SEQ_NUM_SIZE, (unsigned char*)msg, msg_length);
+    memcpy(buf + SEQ_NUM_SIZE, (unsigned char *)msg, msg_length);
 
     // encrypt
     unsigned int encrypted_length;
-    unsigned char* encrypted = symmetric_encrypt_authenticate(
+    unsigned char *encrypted = symmetric_encrypt_authenticate(
         buf, SEQ_NUM_SIZE + msg_length, session_ctx->s_key.mac_key,
         MAC_KEY_SIZE, session_ctx->s_key.cipher_key, CIPHER_KEY_SIZE,
         AES_CBC_128_IV_SIZE, &encrypted_length);
@@ -305,23 +301,23 @@ void send_secure_message(char* msg, unsigned int msg_length,
     session_ctx->sent_seq_num++;
     unsigned char
         sender_buf[MAX_PAYLOAD_LENGTH];  // TODO: Currently the send message
-    // does not support dynamic sizes,
-    // the max length is shorter than
-    // 1024. Must need to decide static
-    // or dynamic buffer size.
+                                         // does not support dynamic sizes,
+                                         // the max length is shorter than
+                                         // 1024. Must need to decide static
+                                         // or dynamic buffer size.
     unsigned int sender_buf_length;
     make_sender_buf(encrypted, encrypted_length, SECURE_COMM_MSG, sender_buf,
-        &sender_buf_length);
+                    &sender_buf_length);
     free(encrypted);
     write(session_ctx->sock, sender_buf, sender_buf_length);
 }
 
-void free_session_key_list_t(session_key_list_t* session_key_list) {
+void free_session_key_list_t(session_key_list_t *session_key_list) {
     free(session_key_list->s_key);
     free(session_key_list);
 }
 
-void free_SST_ctx_t(SST_ctx_t* ctx) {
+void free_SST_ctx_t(SST_ctx_t *ctx) {
     OPENSSL_free(ctx->priv_key);
     OPENSSL_free(ctx->pub_key);
     free_config_t(ctx->config);
