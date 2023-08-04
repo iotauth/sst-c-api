b59338d9841714b82d5dd678ebd4c2f100099bab
yeongbin <baack7700@hanyang.ac.kr>
2023-08-04 17:24:21 +0900

    Revise the code for downloader, uplader code

--- ipfs_examples/entity_downloader.c
+++ ipfs_examples/entity_downloader.c
@@ -5,49 +5,49 @@ int main(int argc, char *argv[]) {
         error_handling("Enter config path");
     }
 
-    // int serv_sock, clnt_sock, clnt_sock2;
-    // const int PORT_NUM = 21100;
-    // struct sockaddr_in serv_addr, clnt_addr;
-    // socklen_t clnt_addr_size;
-    // serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-    // if (serv_sock == -1) {
-    //     error_handling("socket() error");
-    // }
-    // int on = 1;
-    // if (setsockopt(serv_sock, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0) {
-    //     printf("socket option set error\n");
-    //     return -1;
-    // }
-    // memset(&serv_addr, 0, sizeof(serv_addr));
-    // serv_addr.sin_family = AF_INET;
-    // serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    // serv_addr.sin_port = htons(PORT_NUM);
+    int serv_sock, clnt_sock, clnt_sock2;
+    const int PORT_NUM = 21100;
+    struct sockaddr_in serv_addr, clnt_addr;
+    socklen_t clnt_addr_size;
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (serv_sock == -1) {
+        error_handling("socket() error");
+    }
+    int on = 1;
+    if (setsockopt(serv_sock, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0) {
+        printf("socket option set error\n");
+        return -1;
+    }
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(PORT_NUM);
 
-    // if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) ==
-    //     -1) {
-    //     error_handling("bind() error");
-    //     return -1;
-    // }
+    if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) ==
+        -1) {
+        error_handling("bind() error");
+        return -1;
+    }
 
-    // if (listen(serv_sock, 5) == -1) {
-    //     error_handling("listen() error");
-    //     return -1;
-    // }
-    // clnt_addr_size = sizeof(clnt_addr);
-    // clnt_sock =
-    //     accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
-    // if (clnt_sock == -1) {
-    //     error_handling("accept() error");
-    //     return -1;
-    // }
+    if (listen(serv_sock, 5) == -1) {
+        error_handling("listen() error");
+        return -1;
+    }
+    clnt_addr_size = sizeof(clnt_addr);
+    clnt_sock =
+        accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
+    if (clnt_sock == -1) {
+        error_handling("accept() error");
+        return -1;
+    }
 
     char *config_path = argv[1];
     SST_ctx_t *ctx = init_SST(config_path);
 
     INIT_SESSION_KEY_LIST(s_key_list);
     ctx->purpose_index = 0;
-    // SST_session_ctx_t *session_ctx =
-    //     server_secure_comm_setup(ctx, clnt_sock, &s_key_list);
+    SST_session_ctx_t *session_ctx =
+        server_secure_comm_setup(ctx, clnt_sock, &s_key_list);
     sleep(5);
     
     char file_name[BUFF_SIZE];
@@ -58,6 +58,14 @@ int main(int argc, char *argv[]) {
     sleep(5);
     file_download_decrypt(*session_key, &file_name[0]);
 
+    sleep(3);
+    char file_name1[BUFF_SIZE];
+    unsigned char received_skey_id1[SESSION_KEY_ID_SIZE];
+    download_from_file_system_manager(&received_skey_id1[0], ctx, &file_name1[0]);
+    session_key_t *session_key1 =
+        check_sessionkey_request_to_auth(&received_skey_id1[0], ctx, &s_key_list);
+    sleep(5);
+    file_download_decrypt(*session_key1, &file_name1[0]);
 
     // close(clnt_sock2);
     // close(serv_sock);
--- ipfs_examples/entity_uploader.c
+++ ipfs_examples/entity_uploader.c
@@ -5,8 +5,8 @@ int main(int argc, char* argv[]) {
     SST_ctx_t* ctx = init_SST(config_path);
     ctx->purpose_index = 1;
     session_key_list_t* s_key_list_0 = get_session_key(ctx, NULL);
-    // SST_session_ctx_t* session_ctx =
-    //     secure_connect_to_server(&s_key_list_0->s_key[0], ctx);
+    SST_session_ctx_t* session_ctx =
+        secure_connect_to_server(&s_key_list_0->s_key[0], ctx);
     sleep(1);
     char* my_file_path = argv[2];
     unsigned char hash_value[BUFF_SIZE];
@@ -14,8 +14,15 @@ int main(int argc, char* argv[]) {
     // print_buf(s_key_list_0->s_key[0])
     sleep(1);
     upload_to_file_system_manager(&s_key_list_0->s_key[0], ctx, &hash_value[0], hash_value_len);
+    sleep(5);
 
-    // free(session_ctx);
+    unsigned char hash_value1[BUFF_SIZE];
+    int hash_value_len1 = file_encrypt_upload(&s_key_list_0->s_key[1], ctx, my_file_path, &hash_value1[0]);
+    // print_buf(s_key_list_0->s_key[0])
+    sleep(1);
+    upload_to_file_system_manager(&s_key_list_0->s_key[1], ctx, &hash_value1[0], hash_value_len1);
+
+    free(session_ctx);
 
     free_SST_ctx_t(ctx);
 
