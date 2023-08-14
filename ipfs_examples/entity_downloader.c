25dd6117c9a7f078fcc028ac3f3456ccb5a89388
yeongbin <baack7700@hanyang.ac.kr>
2023-08-14 17:42:34 +0900

    Revise the code for feedback

--- ipfs_examples/entity_downloader.c
+++ ipfs_examples/entity_downloader.c
@@ -13,8 +13,7 @@ int main(int argc, char *argv[]) {
     char file_name[BUFF_SIZE];
     unsigned char received_skey_id[SESSION_KEY_ID_SIZE];
     download_from_file_system_manager(&received_skey_id[0], ctx, &file_name[0]);
-    session_key_t *session_key =
-        check_sessionkey_from_key_list(&received_skey_id[0], ctx, &s_key_list);
+    session_key_t *session_key = get_session_key_by_ID(&received_skey_id[0], ctx, &s_key_list);
     sleep(5);
     file_download_decrypt(*session_key, &file_name[0]);
 
@@ -22,8 +21,7 @@ int main(int argc, char *argv[]) {
     char file_name1[BUFF_SIZE];
     unsigned char received_skey_id1[SESSION_KEY_ID_SIZE];
     download_from_file_system_manager(&received_skey_id1[0], ctx, &file_name1[0]);
-    session_key_t *session_key1 =
-        check_sessionkey_from_key_list(&received_skey_id1[0], ctx, &s_key_list);
+    session_key_t *session_key1 = get_session_key_by_ID(&received_skey_id1[0], ctx, &s_key_list);
     sleep(5);
     file_download_decrypt(*session_key1, &file_name1[0]);
 
--- ipfs_examples/entity_uploader.c
+++ ipfs_examples/entity_uploader.c
@@ -5,10 +5,17 @@ int main(int argc, char* argv[]) {
     char* my_file_path = argv[2];
     char* add_reader_path = argv[3];
     SST_ctx_t* ctx = init_SST(config_path);
-
-    // request the adding reader request to Auth without distribution key
-    ctx->purpose_index = 2;
-    send_add_reader_req_via_TCP(ctx, add_reader_path);
+    
+    FILE* add_reader_file = fopen(add_reader_path,"r");
+    char addReader[64];
+    if (add_reader_file == NULL) {
+        error_handling("Cannot open file.\n");
+		exit(1);
+	}
+    while(fgets(addReader, sizeof(addReader), add_reader_file) != NULL) {
+        send_add_reader_req_via_TCP(ctx, addReader);
+    }
+    fclose(add_reader_file);
 
     ctx->purpose_index = 1;
     session_key_list_t* s_key_list_0 = get_session_key(ctx, NULL);
@@ -23,9 +30,6 @@ int main(int argc, char* argv[]) {
     int hash_value_len1 = file_encrypt_upload(&s_key_list_0->s_key[1], ctx, my_file_path, &hash_value1[0]);
     sleep(1);
     upload_to_file_system_manager(&s_key_list_0->s_key[1], ctx, &hash_value1[0], hash_value_len1);
-
-    ctx->purpose_index = 2;
-    send_add_reader_req_via_TCP(ctx, add_reader_path);
 
     free_SST_ctx_t(ctx);
 }
