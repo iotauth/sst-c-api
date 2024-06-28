b03f42e5d52052984469df493201dacded9fb02d
Dongha Kim <dongha@asu.edu>
2024-06-27 21:43:20 -0700

    Add mutex lock to multi_thead test.

--- tests/multi_thread_get_session_key.c
+++ tests/multi_thread_get_session_key.c
@@ -9,7 +9,9 @@ void *send_request(void *SST_ctx) {
     SST_ctx_t *ctx = (SST_ctx_t *)SST_ctx;
     session_key_list_t *s_key_list;
     for(int i = 0; i < MAX_SESSION_KEY; i ++) {
+        pthread_mutex_lock(&ctx->mutex);
         s_key_list = get_session_key(ctx, s_key_list);
+        pthread_mutex_unlock(&ctx->mutex);
     }
     free_session_key_list_t(s_key_list);
 }
@@ -20,6 +22,7 @@ int main(int argc, char *argv[]) {
 
     // Request one session key to just ensure got distribution key.
     session_key_list_t *s_key_list = get_session_key(ctx, NULL);
+    sleep(2);
     int num_threads = 100;
     pthread_t thread[num_threads];
     for (int i = 0; i < 100; i++) {
