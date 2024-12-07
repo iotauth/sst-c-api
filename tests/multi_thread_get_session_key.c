2ae7c55690c792452c22662dfc0aaf63eb385636
Dongha Kim <dongha@asu.edu>
2024-12-07 16:46:32 -0700

    Add more configs and tests.

--- tests/multi_thread_get_session_key.c
+++ tests/multi_thread_get_session_key.c
@@ -7,13 +7,13 @@
 
 void *send_request(void *SST_ctx) {
     SST_ctx_t *ctx = (SST_ctx_t *)SST_ctx;
-    session_key_list_t *s_key_list;
-    for(int i = 0; i < MAX_SESSION_KEY; i ++) {
-        pthread_mutex_lock(&ctx->mutex);
+    for (int i = 0; i < 1; i++) {
+        // pthread_mutex_lock(&ctx->mutex);
+        session_key_list_t *s_key_list;
         s_key_list = get_session_key(ctx, s_key_list);
-        pthread_mutex_unlock(&ctx->mutex);
+        // pthread_mutex_unlock(&ctx->mutex);
+        // free_session_key_list_t(s_key_list);
     }
-    free_session_key_list_t(s_key_list);
 }
 
 int main(int argc, char *argv[]) {
@@ -23,12 +23,13 @@ int main(int argc, char *argv[]) {
     // Request one session key to just ensure got distribution key.
     session_key_list_t *s_key_list = get_session_key(ctx, NULL);
     sleep(2);
-    int num_threads = 100;
+    int num_threads = 5;
     pthread_t thread[num_threads];
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < num_threads; i++) {
         pthread_create(&thread[i], NULL, &send_request, (void *)ctx);
+        // sleep(1); // If this exits, this doesn't fail.
     }
-    for (int i = 0; i< 100; i++) {
+    for (int i = 0; i < 100; i++) {
         pthread_join(thread[i], NULL);
     }
     free_session_key_list_t(s_key_list);
