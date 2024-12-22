be025e3cb099b882df474c0f6915d6d887d7e4c9
Dongha Kim <dongha@asu.edu>
2024-12-22 08:42:44 -0700

    Add comments fo password_test.

--- tests/password_test.c
+++ tests/password_test.c
@@ -1,6 +1,6 @@
 /**
- *  This test mainly tests save_session_key_list_with_password and
- * load_session_key_list_with_password. It gets a session key, saves it,
+ *  This test mainly tests save_session_key_list_with_password() and
+ * load_session_key_list_with_password(). It gets a session key, saves it,
  * encrypts a string, load the saved key, and decrypts the string.
  */
 
@@ -9,8 +9,8 @@
 #include <string.h>
 
 #include "../c_api.h"
-#include "../c_crypto.h"
 #include "../c_common.h"
+#include "../c_crypto.h"
 
 int main(int argc, char *argv[]) {
     char *config_path = argv[1];
@@ -22,44 +22,46 @@ int main(int argc, char *argv[]) {
     char password[] = "examplepassword";
     char salt[] = "randomsalt";
 
+    // Save the session key list with a password, and salt it.
     save_session_key_list_with_password(s_key_list, "./sessionkey", password,
                                         sizeof(password), salt, sizeof(salt));
 
     // Generate buffer
-    unsigned char plaintext[] = "Hello World!";
+    unsigned char plaintext[] =
+        "Testing save_session_key_list_with_password().";
     printf("Plaintext Length: %ld, Plaintext: %s\n",
            strlen((const char *)plaintext), plaintext);
-    int s;
+    int ret;
     unsigned int encrypted_length;
     unsigned char *encrypted;
-    s = symmetric_encrypt_authenticate(
+    ret = symmetric_encrypt_authenticate(
         plaintext, strlen((const char *)plaintext), s_key_list->s_key->mac_key,
         MAC_KEY_SHA256_SIZE, s_key_list->s_key->cipher_key,
         AES_128_KEY_SIZE_IN_BYTES, AES_128_CBC_IV_SIZE, AES_128_CTR, 1,
         &encrypted, &encrypted_length);
-    assert(s == 0);
+    assert(ret == 0);
     printf("Cipher Length: %d, Cipher Text: ", encrypted_length);
     print_buf(encrypted, encrypted_length);
 
     session_key_list_t *new_s_key_list = init_empty_session_key_list();
+
+    // Test to load the saved session key using the password.
     load_session_key_list_with_password(new_s_key_list, "./sessionkey",
                                         password, sizeof(password), salt,
                                         sizeof(salt));
     unsigned int decrypted_length;
     unsigned char *decrypted;
-    s = symmetric_decrypt_authenticate(
+    ret = symmetric_decrypt_authenticate(
         encrypted, encrypted_length, new_s_key_list->s_key->mac_key,
         MAC_KEY_SHA256_SIZE, new_s_key_list->s_key->cipher_key,
         AES_128_KEY_SIZE_IN_BYTES, AES_128_CBC_IV_SIZE, AES_128_CTR, 1,
         &decrypted, &decrypted_length);
     printf("Decrypted Length: %d, Decrypted: %s\n", decrypted_length,
            decrypted);
-    assert(s == 0);
+    assert(ret == 0);
     assert(decrypted_length == strlen((const char *)plaintext));
     assert(strncmp((const char *)decrypted, (const char *)plaintext,
                    decrypted_length) == 0);
-    printf("\n");
-
     free_session_key_list_t(s_key_list);
     free_SST_ctx_t(ctx);
 }
