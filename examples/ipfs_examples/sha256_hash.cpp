69e966321dd2fae30e0bada695fd04733e585cc5
Carlos Beltran <cjbeltr4@asu.edu>
2025-03-17 13:03:50 -0700

    Changed to EVP functions and fixed digest_message_SHA_256()

--- examples/ipfs_examples/sha256_hash.cpp
+++ examples/ipfs_examples/sha256_hash.cpp
@@ -3,17 +3,58 @@
 extern "C" {
 #include <sst-c-api/c_api.h>
 }
-
+// 1. Replace the SHA functions with EVP
+// 2. Check entity uploader and downloader files X
+//      Understand what it does
+// 3. Read the README for examples/file_sharing
 #include <iostream>
 #include <openssl/sha.h>
 #include <iomanip>
 #include <sstream>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include "../../c_crypto.c"
+
+
+#define MAX_ERROR_MESSAGE_LENGTH 128
+
+void print_last_error(char *msg) {
+    char err[MAX_ERROR_MESSAGE_LENGTH];
+
+    ERR_load_crypto_strings();
+    ERR_error_string(ERR_get_error(), err);
+    printf("%s ERROR: %s\n", msg, err);
+    exit(1);
+}
+
 std::string sha256(const std::string& input) {
     unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256_CTX sha256;
-    SHA256_Init(&sha256);
-    SHA256_Update(&sha256, input.c_str(), input.size());
-    SHA256_Final(hash, &sha256);
+    // SHA256_CTX sha256;
+    // EVP_DigestInit_ex(&sha256);
+    // EVP_DigestUpdate(&sha256, input.c_str(), input.size());
+    // EVP_DigestFinal_ex(hash, &sha256);
+
+    unsigned char* md5_hash;
+    unsigned int* md_len;
+
+    digest_message_SHA_256(input.c_str(), input.size(), md5_hash, md_len) // This function pretty much does the same as what this function is doing (so far)
+
+    EVP_MD_CTX *mdctx;
+
+    if ((mdctx = EVP_MD_CTX_create()) == NULL) {
+        print_last_error("EVP_MD_CTX_create() failed");
+    }
+    if (EVP_DigestInit_ex(mdctx, EVP_sha256(), NULL) != 1) {
+        print_last_error("EVP_DigestInit_ex failed");
+    }
+    if (EVP_DigestUpdate(mdctx, input.c_str(), input.size()) != 1) {
+        print_last_error("EVP_DigestUpdate failed");
+    }
+    if (EVP_DigestFinal_ex(mdctx, md5_hash, md_len) != 1) {
+        print_last_error("failed");
+    }
+
+
     std::stringstream ss;
     for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
         ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
