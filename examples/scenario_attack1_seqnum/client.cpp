248b91c535a99d8d5abcf64e4d0e462cb00f53b8
Carlos Beltran <cjbeltr4@asu.edu>
2025-07-14 13:44:20 -0700

    Created First Attack Scenario Example.

---
+++ examples/scenario_attack1_seqnum/client.cpp
@@ -0,0 +1,60 @@
+extern "C" {
+#include <c/c_api.h>
+#include <c/ipfs.h>
+}
+
+#include <unistd.h>
+
+#include <fstream>
+#include <iostream>
+#include <thread>
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <config_path> <csv_file_path>"
+                  << std::endl;
+        exit(1);
+    }
+
+    // Standard SST initialization
+    char *config_path = argv[1];
+    SST_ctx_t *ctx = init_SST(config_path);
+
+    session_key_list_t *s_key_list = get_session_key(ctx, NULL);
+    if (s_key_list == NULL) {
+        std::cerr << "Failed to get session key. Returning NULL.\n"
+                  << ::std::endl;
+        exit(1);
+    }
+
+    // SST Connect to server
+    SST_session_ctx_t *session_ctx =
+        secure_connect_to_server(&s_key_list->s_key[0], ctx);
+
+    // Read CSV file
+    std::string my_file_path = argv[2];
+    std::ifstream file(my_file_path);
+    std::string line;
+
+    // Because client sends messages, changing the received sequence number has no effect
+    // session_ctx->received_seq_num = 10; // Attack the received sequence number
+    // session_ctx->sent_seq_num = 10; // Attack the sent sequence number
+
+    while (std::getline(file, line)) {
+        // Sleep for the specified time
+        std::string sleep_time_str = line.substr(0, line.find(','));
+        int sleep_time = std::stoi(sleep_time_str);
+        std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
+
+        // Send message to server
+        std::string message = line.substr(line.find(',') + 1);
+        send_secure_message(const_cast<char *>(message.c_str()),
+                            message.length(), session_ctx);
+    }
+
+    free(session_ctx);
+    free_session_key_list_t(s_key_list);
+    free_SST_ctx_t(ctx);
+
+    return EXIT_SUCCESS;
+}
---
+++ examples/scenario_attack1_seqnum/server.cpp
@@ -0,0 +1,106 @@
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <fstream>
+#include <iostream>
+
+extern "C" {
+#include <c/c_api.h>
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <config_path>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // Initialize the sockets
+    int serv_sock, clnt_sock;
+    const char *PORT_NUM = "21100";
+
+    struct sockaddr_in serv_addr, clnt_addr;
+    socklen_t clnt_addr_size;
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (serv_sock == -1) {
+        std::cerr << "socket() error" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    int on = 1;
+    if (setsockopt(serv_sock, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0) {
+        std::cerr << "socket option set error\n" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(PORT_NUM));
+
+    if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) ==
+        -1) {
+        std::cerr << "bind() error" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    if (listen(serv_sock, 5) == -1) {
+        std::cerr << "listen() error" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    clnt_addr_size = sizeof(clnt_addr);
+    clnt_sock =
+        accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
+    if (clnt_sock == -1) {
+        std::cerr << "accept() error" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    char *config_path = argv[1];
+    SST_ctx_t *ctx = init_SST(config_path);
+    session_key_list_t *s_key_list = init_empty_session_key_list();
+    SST_session_ctx_t *session_ctx =
+        server_secure_comm_setup(ctx, clnt_sock, s_key_list);
+    if (session_ctx == NULL) {
+        std::cerr << "There is no session key.\n" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // Receive messages from client
+    unsigned char *received_buf;
+    unsigned char *received_plaintext;
+
+    // Because server receives messages, changing the sent sequence number has no effect
+    // session_ctx->received_seq_num--; // Attack the received sequence number
+    // session_ctx->sent_seq_num++; // Attack the sent sequence number
+
+    for (;;) {
+        int ret =
+            read_secure_message(session_ctx->sock, &received_buf, session_ctx);
+        if (ret == -1) {
+            std::cerr << "Failed to read secure message." << std::endl;
+            break;
+        } else if (ret == 0) {
+            std::cerr << "No more messages to read." << std::endl;
+            break;
+        }
+
+        // Process the received_buf message
+        // TODO: Remove this temporary fix once the C API is fixed.
+        // Remove the two 4-byte sequence numbers in the received buffer
+        received_plaintext = received_buf + 8;
+        std::cout << reinterpret_cast<const char *>(received_plaintext)
+                  << std::endl;
+    }
+
+    std::cout << "Finished communication." << std::endl;
+
+    if (close(clnt_sock) == -1) {
+        std::cerr << "close() error" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    free(session_ctx);
+    free_SST_ctx_t(ctx);
+    free_session_key_list_t(s_key_list);
+}
