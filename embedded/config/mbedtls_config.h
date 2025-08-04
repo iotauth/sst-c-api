42d6c3a60d5c43257da644d5a53c6204b10f806a
JFelix000 <jmfelix4@asu.edu>
2025-08-04 15:16:26 -0700

    changes needed to overwrite outside of mbedtls/ folder

--- embedded/config/mbedtls_config.h
+++ embedded/config/mbedtls_config.h
@@ -1,27 +1,31 @@
 #ifndef MBEDTLS_CONFIG_H
 #define MBEDTLS_CONFIG_H
 
-// Core modules
+// === Core crypto modules ===
 #define MBEDTLS_AES_C
 #define MBEDTLS_GCM_C
 #define MBEDTLS_CIPHER_C
 
-// Platform specifics for Pico
+// === Platform configuration for embedded (Pico) ===
 #define MBEDTLS_PLATFORM_C
 #define MBEDTLS_PLATFORM_MEMORY
 #define MBEDTLS_NO_PLATFORM_ENTROPY
+
+// === Platform zeroization === use default
+#undef MBEDTLS_PLATFORM_ZEROIZE_ALT
+
+// === Timing and time-related features === not used yet
 #undef MBEDTLS_TIMING_C
-
+//for my own timing override
 #define MBEDTLS_HAVE_TIME
-#define MBEDTLS_PLATFORM_MS_TIME_ALT
+#define MBEDTLS_PLATFORM_TIME_ALT
 
-// Disable unnecessary features
+// === Unused or unnecessary features ===
 #undef MBEDTLS_SELF_TEST
 #undef MBEDTLS_ERROR_C
 #undef MBEDTLS_FS_IO
-#undef MBEDTLS_TIMING_C // remove default time function for override
 
-
+// === Check config consistency ===
 #include "mbedtls/check_config.h"
 
 #endif // MBEDTLS_CONFIG_H
