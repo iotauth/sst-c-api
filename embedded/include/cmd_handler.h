85c3d7aec6dfdb4aad6842b5b8c1469acb934923
josef <jmfelix4@asu.edu>
2025-08-12 13:01:51 -0700

    change value to bool as expected

--- embedded/include/cmd_handler.h
+++ embedded/include/cmd_handler.h
@@ -3,6 +3,6 @@
 
 #include <stdint.h>
 
-void handle_commands(const char *cmd, uint8_t *session_key, int *current_slot);
+bool handle_commands(const char *cmd, uint8_t *session_key, int *current_slot);
 
 #endif // CMD_HANDLER_H
