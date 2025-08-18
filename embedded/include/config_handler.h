b7af873e0dd0d70b6d66c86eb92ecf34e6c7c510
Dongha Kim <dongha@asu.edu>
2025-08-18 09:33:56 -0700

    fix: add missing documentation for config handler functions

--- embedded/include/config_handler.h
+++ embedded/include/config_handler.h
@@ -1,7 +1,17 @@
 #ifndef CONFIG_HANDLER_H
 #define CONFIG_HANDLER_H
 
+// Changes the current working directory to the location of the given config
+// file. If `config_path` is NULL, changes to a default directory.
+//
+// @param config_path Full or relative path to a config file (can be NULL)
 void change_directory_to_config_path(const char* config_path);
+
+// Returns just the filename from a given path, assuming the working directory
+// was already changed correctly.
+//
+// @param path Full or relative path to the config file
+// @return Pointer to static buffer containing filename
 const char* get_config_path(const char* path);
 
 #endif  // CONFIG_HANDLER_H
